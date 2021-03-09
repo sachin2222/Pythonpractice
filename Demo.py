2abf55999c62ae76e26f2382f6962b1c34f2be79
sachin2222 <sachin.vashishth007@gmail.com>
2021-03-09 15:50:23 +0530

    Initial commit

---
+++ Demo.py
@@ -0,0 +1,10 @@
+print("hello world")
+
+a = 10
+print("value is", a)
+print(type(a))
+
+str1 = "this is string1"
+str2 = 'this is strrin2'
+
+print(str1 + ' ' + str2)
---
+++ Demo2.py
@@ -0,0 +1,13 @@
+values = [1, 2, 3, 4, "sachin", 5]
+print(values)
+print(values[0])
+print(values[-1])
+print(values[2:4])
+values.insert(5,"sharma")
+print(values)
+values.append("End value")
+print(values)
+
+#values.remove()
+
+
