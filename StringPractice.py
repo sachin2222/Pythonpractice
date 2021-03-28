e9ccf8179c0304449cf4d84722436404af875673
sachin2222 <sachin.vashishth007@gmail.com>
2021-03-28 17:35:37 +0530

    commit

--- StringPractice.py
+++ StringPractice.py
@@ -1,5 +1,26 @@
-s1 = "Hello World!"
-reverse = ""
-for i in s1:
-    reverse = i + reverse
-print(reverse)
+# reverse = ""
+# for i in s1:
+#     reverse = i + reverse
+#
+# for i in range(len(s1)):
+#     if s1[i] != ' ':
+#         j = 0
+#         count = 0
+#         while j < len(s1) - 1:
+#             if s1[i] == s1[j]:
+#                 count = count + 1
+#             j = j + 1
+#         print(s1[i], count)
+
+# s1 = "Hello World!"
+# frequency = dict()
+# for char in s1:
+#     if char != ' ':
+#         count = s1.count(char)
+#         frequency[char] = count
+#
+# print(frequency)
+str1 = "Emma-is-a-data-scientist"
+subtring = str1.split("-")
+
+print(subtring)
---
+++ StringPractice2.py
@@ -0,0 +1,10 @@
+# str_list = ["Emma", "Jon", "", "Kelly", "sachin", "Eric", ""]
+#
+# str_list.remove("")
+# # print(str_list)
+# str1 = 'I am 25 years and 10 months old'
+# list=[]
+# for char in str1:
+#     if char.isdigit():
+
+
